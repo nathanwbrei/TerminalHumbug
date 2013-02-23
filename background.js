4bf17a88154ed9d82e1c1ba42dd5cd23cf89b8fd
Nathan W Brei <nathan.w.brei@gmail.com>
2013-02-22 19:29:03 -0500

    Removing more unnecessary stuff

--- background.js
+++ background.js
@@ -4,12 +4,8 @@ chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
   	if (request.data) {
   		console.log("Background.js has received a scraping!");
-    	sendResponse({farewell: "Thanks from background.js"});
     	var r = new XMLHttpRequest();
     	r.open('POST', 'http://localhost:5000/humbug', true);
     	r.send(request.data);
     }
-  	else {
-  		console.log("Background.js received something, but something didn't work.");
-  	}
   });
